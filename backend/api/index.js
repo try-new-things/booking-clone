93f6e837b1b26a1add83848f42be25f1202738b9
try-new-things <rudgh8582@gmail.com>
2022-05-10 23:35:37 +0900

    add dbconnect

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,7 +1,26 @@
 import express from 'express';
+import dotenv from 'dotenv';
 
+import createDB from './db/connect';
+import hotels from './routes/hotels';
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 
+// router
+app.use('/api/v1/hotels', hotels);
+
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await createDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+}
